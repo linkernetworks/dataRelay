9f24943af28f3751e509435fb9378e1a8cfa1fe3
Evan Lin <evanslin@gmail.com>
2016-07-07 17:54:37 +0800

    [Implement]: Complete basic data relay function.

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
-dataRelay
+dataRelay: A data relay from Kafka to target address
 
 
 Cassandra version (1.x, 2.x only, not capatible with 3.x)
 
-### Run Cassandra 2.2.5 with docker
+
+
+Options
+---------------
+
+- `server`/`s` : The comma separated list of server could be brokers in the Kafka cluster or spark address.
+- `topic`/`t` : The topic to kafka produce.
+- `dest`/`d` : Destination to `kafka`, `spark`, `cassandra` and `stdout`.
+- `pipe`/`p`: Using pipe mode to forward data. usage `cat data.txt | linkerConnector -p true`
+- `--help` : Display detail command help.
+
+
+Cassandra Data Schema
+---------------
+
+#### Run Cassandra 2.2.5 with docker
 
 ```
 docker run --name cassandra-dap -p 9042:9042 -p 7000:7000 -p 7001:7001 -p 7199:7199 -d cassandra:2.2.5
 ```
-
-
-
-### Cassandra Data Schema
 
 #### Keyspace 
 
@@ -29,4 +40,27 @@ CREATE TABLE test_topic (
 key text, 
 value text, 
 PRIMARY KEY (value));
+```
+
+Installation and Usage
+---------------
+#### Installation:
+
+- `git clone https://github.com/LinkerNetworks/dataRelay `
+- install Golang
+- `cd $GOPATH/src/gihub.com/LinkerNetworks/dataRelay`
+- `go build and go install`
+
+### Usage:
+
+#### Relay Kafka data to Cassandra
+
+```
+dataRelay -s=127.0.0.1:9092 -t=test -d=cassandra
+```
+
+#### Relay Kafka data to Stdout
+
+```
+dataRelay -s=127.0.0.1:9092 -t=test 
 ```
--- main.go
+++ main.go
@@ -1,129 +1,139 @@
 package main
 
+// sender := sendr.NewSender("dataRelay")
+// sender.SendData("cassandra", "127.0.0.1", "dap", "k2", "v2")
+
+// 	cluster := gocql.NewCluster("127.0.0.1")
+// 	cluster.Keyspace = "dap"
+// 	cluster.Consistency = gocql.Quorum
+// 	session, err := cluster.CreateSession()
+// 	if err != nil {
+// 		log.Fatal("fail:", err)
+// 	}
+// 	defer session.Close()
+
+// 	var key, value string
+
+// 	// if err := session.Query(`INSERT INTO test_topic (key, value) VALUES (?, ?)`, "key1", "v1").Exec(); err != nil {
+// 	// 	log.Fatal(err)
+// 	// }
+// 	iter2 := session.Query("SELECT key,value  FROM test_topic").Iter()
+// 	for iter2.Scan(&key, &value) {
+// 		fmt.Println("kafka:", key, value)
+// 	}
+// 	if err := iter2.Close(); err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	var city string
+// 	var age int
+// 	/* Search for a specific set of records whose 'timeline' column matches
+// 	 * the value 'me'. The secondary index that we created earlier will be
+// 	 * used for optimizing the search */
+// 	if err := session.Query("SELECT city, age FROM users").Consistency(gocql.One).Scan(&city, &age); err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println("Tweet:", city, age)
+
+// 	iter := session.Query("SELECT city, age  FROM users").Iter()
+// 	for iter.Scan(&city, &age) {
+// 		fmt.Println("Tweet:", city, age)
+// 	}
+// 	if err := iter.Close(); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"os/signal"
 
 	sendr "github.com/LinkerNetworks/linkerConnector/sender"
-	"github.com/gocql/gocql"
+	"github.com/Shopify/sarama"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	send *sendr.Sender
 )
 
 func main() {
-	// connect to the cluster
-	sender := sendr.NewSender("dataRelay")
-	sender.SendData("cassandra", "127.0.0.1", "dap", "k2", "v2")
+	send = sendr.NewSender("dataRelay")
+	// sender.SendData("cassandra", "127.0.0.1", "dap", "k2", "v2")
 
-	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = "dap"
-	cluster.Consistency = gocql.Quorum
-	session, err := cluster.CreateSession()
-	if err != nil {
-		log.Fatal("fail:", err)
-	}
-	defer session.Close()
+	var targetAddr, serverAddr, topic, dest string
+	var usingPipe bool
+	rootCmd := &cobra.Command{
+		Use:   "dataRelay",
+		Short: "relay Kafka data to target DB system",
+		Run: func(cmd *cobra.Command, args []string) {
+			if usingPipe {
+				info, _ := os.Stdin.Stat()
 
-	var key, value string
+				if info.Size() > 0 {
+					reader := bufio.NewReader(os.Stdin)
+					processPipe(reader, dest, serverAddr, topic)
+				}
+				return
+			}
 
-	// if err := session.Query(`INSERT INTO test_topic (key, value) VALUES (?, ?)`, "key1", "v1").Exec(); err != nil {
-	// 	log.Fatal(err)
-	// }
-	iter2 := session.Query("SELECT key,value  FROM test_topic").Iter()
-	for iter2.Scan(&key, &value) {
-		fmt.Println("kafka:", key, value)
-	}
-	if err := iter2.Close(); err != nil {
-		log.Fatal(err)
+			var serverList []string
+			serverList = append(serverList, serverAddr)
+			c, err := sarama.NewConsumer(serverList, nil)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			pc, err := c.ConsumePartition(topic, 0, sarama.OffsetNewest)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			go func() {
+				signals := make(chan os.Signal, 1)
+				signal.Notify(signals, os.Kill, os.Interrupt)
+				<-signals
+				pc.AsyncClose()
+			}()
+
+			for {
+				for msg := range pc.Messages() {
+					fmt.Printf("Offset: %d\n", msg.Offset)
+					fmt.Printf("Key:    %s\n", string(msg.Key))
+					fmt.Printf("Value:  %s\n", string(msg.Value))
+					fmt.Println()
+					send.SendData(sendr.SendDataParam{Dest: dest, SerAddr: "127.0.0.1", Topic: "dap", Key: string(msg.Key), Value: string(msg.Value), Table: "test_topic"})
+				}
+
+				if err := c.Close(); err != nil {
+					fmt.Println("Failed to close consumer: ", err)
+				}
+			}
+		},
 	}
 
-	var city string
-	var age int
-	/* Search for a specific set of records whose 'timeline' column matches
-	 * the value 'me'. The secondary index that we created earlier will be
-	 * used for optimizing the search */
-	if err := session.Query("SELECT city, age FROM users").Consistency(gocql.One).Scan(&city, &age); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Tweet:", city, age)
+	rootCmd.Flags().BoolVarP(&usingPipe, "pipe", "p", false, "Using pipe mode to forward data")
+	rootCmd.Flags().StringVarP(&serverAddr, "source", "s", "", "Kafka server and port")
+	rootCmd.Flags().StringVarP(&targetAddr, "target", "g", "", "Target server address and port")
+	rootCmd.Flags().StringVarP(&topic, "topic", "t", "", "The topic to kafka consume")
+	rootCmd.Flags().StringVarP(&dest, "dest", "d", "stdout", "Destination to cassandra or kafka/spark/cassandra/stdout")
+	rootCmd.Execute()
 
-	iter := session.Query("SELECT city, age  FROM users").Iter()
-	for iter.Scan(&city, &age) {
-		fmt.Println("Tweet:", city, age)
-	}
-	if err := iter.Close(); err != nil {
-		log.Fatal(err)
-	}
 }
 
-// package main
-
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"io"
-// 	"os"
-// 	"runtime"
-// 	"time"
-
-// 	"github.com/spf13/cobra"
-// 	sendr "gihub.com/LinkerNetworks/linkerConnector/sender"
-// )
-
-// func main() {
-// 	var targetAddr, serverAddr, topic, dest string
-// 	var interval int
-// 	var usingPipe bool
-// 	rootCmd := &cobra.Command{
-// 		Use:   "dataRelay",
-// 		Short: "relay Kafka data to target DB system",
-// 		Run: func(cmd *cobra.Command, args []string) {
-// 			if usingPipe {
-// 				info, _ := os.Stdin.Stat()
-
-// 				if info.Size() > 0 {
-// 					reader := bufio.NewReader(os.Stdin)
-// 					processPipe(reader, dest, serverAddr, topic)
-// 				}
-// 				return
-// 			}
-// 			if runtime.GOOS != "linux" {
-// 				fmt.Println("Collect data from linux for now, application exit.")
-// 				return
-// 			}
-
-// 			for {
-// 				data := NewDataCollector()
-// 				procInfo := data.GetProcessInfo()
-// 				machineInfo := data.GetMachineInfo()
-
-// 				sendData(dest, serverAddr, topic, "ProcessInfo", procInfo)
-// 				sendData(dest, serverAddr, topic, "MachineInfo", machineInfo)
-
-// 				if interval == 0 {
-// 					return
-// 				}
-
-// 				time.Sleep(time.Millisecond * time.Duration(interval))
-// 			}
-// 		},
-// 	}
-
-// 	rootCmd.Flags().StringVarP(&serverAddr, "server", "s", "", "Kafka server and port")
-// 	rootCmd.Flags().StringVarP(&targetAddr, "target", "ts", "", "Target server address and port")
-// 	rootCmd.Flags().StringVarP(&topic, "topic", "t", "", "The topic to kafka consume")
-// 	rootCmd.Flags().StringVarP(&dest, "dest", "d", "stdout", "Destination to cassandra or kafka")
-// 	rootCmd.Execute()
-
-// }
-
-// func processPipe(reader *bufio.Reader, dest, serverAddr, topic string) {
-// 	line := 1
-// 	for {
-// 		input, err := reader.ReadString('\n')
-// 		if err != nil && err == io.EOF {
-// 			break
-// 		}
-
-// 		sendData(dest, serverAddr, topic, "Pipe", input)
-// 		line++
-// 	}
-// }
+func processPipe(reader *bufio.Reader, dest, serverAddr, topic string) {
+	line := 1
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil && err == io.EOF {
+			break
+		}
+		send.SendData(sendr.SendDataParam{Dest: dest, SerAddr: serverAddr, Topic: topic, Key: "Pipe", Value: input})
+		line++
+	}
+}
